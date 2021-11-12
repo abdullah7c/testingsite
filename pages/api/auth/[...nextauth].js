98513ae41f415016fe01813bf5af64bf87543990
Syed_Zaidi <syedabdullah.7ctech@gmail.com>
2021-11-12 08:11:43 -0800

    updated

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -4,64 +4,68 @@ import CredentialsProvider from 'next-auth/providers/credentials'
 const maxAged = 60*60;
 
 export default NextAuth({
+//   providers: [
+//     CredentialsProvider({
+//       // The name to display on the sign in form (e.g. 'Sign in with...')
+//       name: 'Credentials',
+//       // The credentials is used to generate a suitable form on the sign in page.
+//       // You can specify whatever fields you are expecting to be submitted.
+//       // e.g. domain, username, password, 2FA token, etc.
+//       // You can pass any HTML attribute to the <input> tag through the object.
+//       credentials: {
+//         username: { label: "Username", type: "text", placeholder: "jsmith" },
+//         password: {  label: "Password", type: "password" }
+//       },
+//       async authorize(credentials, req) {
+//         // Add logic here to look up the user from the credentials supplied
+//         const user = { id: 1, name: 'J Smith', email: 'jsmith@example.com' }
+  
+//         if (
+//           credentials.username === "abdullah@test.com" &&
+//           credentials.password === "7ctech"
+//         ) {
+//           // Any object returned will be saved in `user` property of the JWT
+//           return user
+//         } else {
+//           // If you return null or false then the credentials will be rejected
+//           return null
+//           // You can also Reject this callback with an Error or with a URL:
+//           // throw new Error('error message') // Redirect to error page
+//           // throw '/path/to/redirect'        // Redirect to a URL
+//         }
+//       }
+//     })
+//   ],
   providers: [
     CredentialsProvider({
-      // The name to display on the sign in form (e.g. 'Sign in with...')
       name: 'Credentials',
-      // The credentials is used to generate a suitable form on the sign in page.
-      // You can specify whatever fields you are expecting to be submitted.
-      // e.g. domain, username, password, 2FA token, etc.
-      // You can pass any HTML attribute to the <input> tag through the object.
       credentials: {
-        username: { label: "Username", type: "text", placeholder: "jsmith" },
+        username: {
+            label: "Email",
+            type: "email",
+            placeholder: "Username/Email",
+          },
         password: {  label: "Password", type: "password" }
       },
-      async authorize(credentials, req) {
-        // Add logic here to look up the user from the credentials supplied
-        const user = { id: 1, name: 'J Smith', email: 'jsmith@example.com' }
-  
-        if (
-          credentials.username === "abdullah@test.com" &&
-          credentials.password === "7ctech"
-        ) {
-          // Any object returned will be saved in `user` property of the JWT
-          return user
-        } else {
-          // If you return null or false then the credentials will be rejected
-          return null
-          // You can also Reject this callback with an Error or with a URL:
-          // throw new Error('error message') // Redirect to error page
-          // throw '/path/to/redirect'        // Redirect to a URL
+      authorize: (credentials) => {
+
+        try {
+            if(credentials.username === "abdullah@test.com" &&
+            credentials.password === "7ctech"){
+                return {
+                    id: 2,
+                    name: "abdullah",
+                    email: "abdullah@test.com",
+                  };
+            }
+          
+        } catch (error) {
+            return null;
         }
-      }
+        
+      },
     })
   ],
-  // providers: [
-  //   CredentialsProvider({
-  //     name: 'Credentials',
-  //     credentials: {
-  //       username: {
-  //           label: "Email",
-  //           type: "email",
-  //           placeholder: "Username/Email",
-  //         },
-  //       password: {  label: "Password", type: "password" }
-  //     },
-  //     authorize: (credentials) => {
-  //       if (
-  //         credentials.username === "abdullah@test.com" &&
-  //         credentials.password === "7ctech"
-  //       ) {
-  //         return {
-  //           id: 2,
-  //           name: "abdullah",
-  //           email: "abdullah@test.com",
-  //         };
-  //       }
-  //       return null;
-  //     },
-  //   })
-  // ],
   callbacks: {
     jwt: ({ token, user }) => {
       if (user) {
